6519772002994629fd5f63e28365ebf89edc3601
«Amir» <benoevsky.1@mail.ru>
2024-08-16 13:07:39 +0300

    fix

--- src/items.ts
+++ src/items.ts
@@ -1,42 +1,42 @@
 export const items = [
   {
     id: 1,
-    img: "./assets/avocado.png",
+    img: "/assets/avocado.png",
     matched: false,
   },
   {
     id: 2,
-    img: "./assets/burger.png",
+    img: "/assets/burger.png",
     matched: false,
   },
   {
     id: 3,
-    img: "./assets/cake.png",
+    img: "/assets/cake.png",
     matched: false,
   },
   {
     id: 4,
-    img: "./assets/cheese.png",
+    img: "/assets/cheese.png",
     matched: false,
   },
   {
     id: 5,
-    img: "./assets/donut.png",
+    img: "/assets/donut.png",
     matched: false,
   },
   {
     id: 6,
-    img: "./assets/pizza.png",
+    img: "/assets/pizza.png",
     matched: false,
   },
   {
     id: 7,
-    img: "./assets/pumpkin.png",
+    img: "/assets/pumpkin.png",
     matched: false,
   },
   {
     id: 8,
-    img: "./assets/watermelon.png",
+    img: "/assets/watermelon.png",
     matched: false,
   },
 ];
